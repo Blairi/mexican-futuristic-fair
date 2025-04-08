5a45e6fe3a9b4a81a254817ea9bb595dfd4363f1
Blairi <axel.avi.fer@gmail.com>
2025-04-08 14:21:29 -0600

    cambiando nombre del window

--- mexican-futuristic-fair/Window.cpp
+++ mexican-futuristic-fair/Window.cpp
@@ -36,7 +36,7 @@ int Window::Initialise()
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//CREAR VENTANA
-	mainWindow = glfwCreateWindow(width, height, "Practica 07: Iluminacion 1", NULL, NULL);
+	mainWindow = glfwCreateWindow(width, height, "Proyecto Final. Feria Mexicana Futurista", NULL, NULL);
 
 	if (!mainWindow)
 	{
