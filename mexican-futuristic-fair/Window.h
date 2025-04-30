7e581e689f4ec170ab9524feba18a3da549f04c2
Blairi <axel.avi.fer@gmail.com>
2025-04-29 20:53:23 -0600

    seleccion de personaje

--- mexican-futuristic-fair/Window.h
+++ mexican-futuristic-fair/Window.h
@@ -18,7 +18,8 @@ public:
 		return  glfwWindowShouldClose(mainWindow);}
 	bool* getsKeys() { return keys; }
 	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
-	
+	int getIdPersonaje() { return idPersonaje; }
+	bool isPersonajeSeleccionado(){ return personajeSeleccionado; }
 	~Window();
 private: 
 	GLFWwindow *mainWindow;
@@ -31,6 +32,11 @@ private:
 	GLfloat xChange;
 	GLfloat yChange;
 	GLfloat muevex;
+
+	int idPersonaje;
+	int NUMERO_PERSONAJES;
+	bool personajeSeleccionado;
+
 	bool mouseFirstMoved;
 	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
 	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
