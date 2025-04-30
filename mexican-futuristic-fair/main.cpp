12cef0f6ed7904a938947f474eb7a7eb99039b61
Blairi <axel.avi.fer@gmail.com>
2025-04-29 20:53:46 -0600

    renderizando letras

--- mexican-futuristic-fair/main.cpp
+++ mexican-futuristic-fair/main.cpp
@@ -11,6 +11,7 @@ Autores:
 #include <cmath>
 #include <vector>
 #include <math.h>
+#include <map>
 
 #include <glew.h>
 #include <glfw3.h>
@@ -50,7 +51,16 @@ Texture plainTexture;
 Texture pisoTexture;
 Texture AgaveTexture;
 
-Model Invincible_M;
+Texture ProjectDefaultFont;
+
+Model AtomEve_torso;
+Model AtomEve_cabeza;
+Model AtomEve_brazo_der;
+Model AtomEve_brazo_izq;
+Model AtomEve_pie_der;
+Model AtomEve_pie_izq;
+
+Model Base;
 
 Skybox skybox;
 
@@ -106,6 +116,25 @@ void calcAverageNormals(unsigned int* indices, unsigned int indiceCount, GLfloat
 }
 
 
+void createInterface()
+{
+	unsigned int letrasIndices[] = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+
+	GLfloat letrasVertices[] = {
+		-0.5f, 0.0f, 0.5f,		0.0f, 0.8164f,		0.0f, -1.0f, 0.0f,
+		0.5f, 0.0f, 0.5f,		0.1230f, 0.8164f,		0.0f, -1.0f, 0.0f,
+		0.5f, 0.0f, -0.5f,		0.1230f, 0.9707f,		0.0f, -1.0f, 0.0f,
+		-0.5f, 0.0f, -0.5f,		0.0f, 0.9707f,		0.0f, -1.0f, 0.0f,
+	};
+
+	Mesh* obj5 = new Mesh();
+	obj5->CreateMesh(letrasVertices, letrasIndices, 32, 6);
+	meshList.push_back(obj5);
+}
+
 void CreateObjects()
 {
 	unsigned int indices[] = {
@@ -152,8 +181,6 @@ void CreateObjects()
 		0.0f, -0.5f, 0.5f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
 		0.0f, 0.5f, 0.5f,		1.0f, 1.0f,		0.0f, 0.0f, 0.0f,
 		0.0f, 0.5f, -0.5f,		0.0f, 1.0f,		0.0f, 0.0f, 0.0f,
-
-
 	};
 	
 	Mesh *obj1 = new Mesh();
@@ -192,6 +219,7 @@ int main()
 	mainWindow = Window(1920, 1070); // 1280, 1024 or 1024, 768
 	mainWindow.Initialise();
 
+	createInterface();
 	CreateObjects();
 	CreateShaders();
 
@@ -208,8 +236,24 @@ int main()
 	AgaveTexture = Texture("Textures/Agave.tga");
 	AgaveTexture.LoadTextureA();
 
-	Invincible_M = Model();
-	Invincible_M.LoadModel("Models/model_invincible.obj");
+	ProjectDefaultFont = Texture("Textures/fuente-proy.png");
+	ProjectDefaultFont.LoadTextureA();
+
+	Base = Model();
+	Base.LoadModel("Models/base.obj");
+
+	AtomEve_torso = Model();
+	AtomEve_torso.LoadModel("Models/AtomEve/Body.obj");
+	AtomEve_cabeza = Model();
+	AtomEve_cabeza.LoadModel("Models/AtomEve/Head.obj");
+	AtomEve_brazo_der = Model();
+	AtomEve_brazo_der.LoadModel("Models/AtomEve/ArmR.obj");
+	AtomEve_brazo_izq = Model();
+	AtomEve_brazo_izq.LoadModel("Models/AtomEve/ArmL.obj");
+	AtomEve_pie_der = Model();
+	AtomEve_pie_der.LoadModel("Models/AtomEve/LegR.obj");
+	AtomEve_pie_izq = Model();
+	AtomEve_pie_izq.LoadModel("Models/AtomEve/LegL.obj");
 
 	std::vector<std::string> skyboxFaces;
 	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_rt.tga");
@@ -272,10 +316,48 @@ int main()
 	//se crean mas luces puntuales y spotlight 
 
 	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
-		uniformSpecularIntensity = 0, uniformShininess = 0;
+		uniformSpecularIntensity = 0, uniformShininess = 0, uniformTextureOffset = 0;;
 	GLuint uniformColor = 0;
 	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
 	////Loop mientras no se cierra la ventana
+	int idPersonaje = 0;
+	float rotacionCamara = 0.0f;
+	float offsetFuenteProyU = 0.0f;
+	//float offsetFuenteProyV = 0.8164f;
+	float offsetFuenteProyV = 0.8164f;
+
+	std::map<char, std::pair<float, float>> letrasOffset = {
+		{ 'A', std::make_pair(0.0f, 0.8164f) },
+		{ 'B', std::make_pair(0.125f, 0.8164f) },
+		{ 'C', std::make_pair(0.2324f, 0.8164f) },
+		{ 'D', std::make_pair(0.34375f, 0.8164f) },
+		{ 'E', std::make_pair(0.4589f, 0.8164f) },
+		{ 'F', std::make_pair(0.5550f, 0.8164f) },
+		{ 'G', std::make_pair(0.6757f, 0.8164f) },
+		{ 'H', std::make_pair(0.7890f, 0.8164f) },
+
+		{ 'I', std::make_pair(0.0136f, 0.6015f) },
+		{ 'J', std::make_pair(0.1191f, 0.6015f) },
+		{ 'K', std::make_pair(0.2324f, 0.6015f) },
+		{ 'L', std::make_pair(0.3515f, 0.6015f) },
+		{ 'M', std::make_pair(0.4511f, 0.6015f) },
+		{ 'N', std::make_pair(0.5664f, 0.6015f) },
+		{ 'O', std::make_pair(0.7871f, 0.6015f) },
+
+		{ 'P', std::make_pair(0.0175f, 0.3847f) },
+		{ 'Q', std::make_pair(0.1210f, 0.3847f) },
+		{ 'R', std::make_pair(0.2343f, 0.3847f) },
+		{ 'S', std::make_pair(0.3457f, 0.3847f) },
+		{ 'T', std::make_pair(0.4492f, 0.3847f) },
+		{ 'U', std::make_pair(0.5703f, 0.3847f) },
+		{ 'V', std::make_pair(0.6718f, 0.3847f) },
+		{ 'W', std::make_pair(0.7832f, 0.3847f) },
+
+		{ 'X', std::make_pair(0.0f, 0.1679f) },
+		{ 'Y', std::make_pair(0.1171f, 0.1679f) },
+		{ 'Z', std::make_pair(0.2382f, 0.1679f) },
+	};
+
 	while (!mainWindow.getShouldClose())
 	{
 		GLfloat now = glfwGetTime();
@@ -285,8 +367,19 @@ int main()
 
 		//Recibir eventos del usuario
 		glfwPollEvents();
-		camera.keyControl(mainWindow.getsKeys(), deltaTime);
-		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+
+		rotacionCamara += 0.005f * deltaTime;
+
+		if (mainWindow.isPersonajeSeleccionado()) {
+			camera.keyControl(mainWindow.getsKeys(), deltaTime);
+			camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+		}
+		else {
+			float x = 100 * cos(rotacionCamara);
+			float z = 100 * sin(rotacionCamara);
+			camera.setPosition(glm::vec3(x, 7.0f, z));
+			camera.setDirection(glm::vec3(-x, 0.0f, -z));
+		}
 
 		// Clear the window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -298,6 +391,7 @@ int main()
 		uniformView = shaderList[0].GetViewLocation();
 		uniformEyePosition = shaderList[0].GetEyePositionLocation();
 		uniformColor = shaderList[0].getColorLocation();
+		uniformTextureOffset = shaderList[0].getOffsetLocation();
 		
 		//información en el shader de intensidad especular y brillo
 		uniformSpecularIntensity = shaderList[0].GetSpecularIntensityLocation();
@@ -319,31 +413,104 @@ int main()
 		shaderList[0].SetPointLights(pointLights, pointLightCount);
 		shaderList[0].SetSpotLights(spotLights, spotLightCount);
 
+		/*
+		* eleccion de personaje
+		*/
+		if (!mainWindow.isPersonajeSeleccionado()) {
+			idPersonaje = mainWindow.getIdPersonaje();
+		}
+
 		glm::mat4 model(1.0);
 		glm::mat4 modelaux(1.0);
 		glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
+		glm::vec2 toffset = glm::vec2(0.0f, 0.0f);
+		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
 
 		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(0.0f, -1.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		Base.RenderModel();
 
-		pisoTexture.UseTexture();
-		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		// variables auxiliares para la interfaz de selección de personaje
+		glm::vec3 camPos = camera.getCameraPosition();
+		glm::vec3 camFront = glm::normalize(camera.getCameraDirection());
+		glm::vec3 modeloPos = camPos + camFront * 20.0f;
 
-		meshList[2]->RenderMesh();
-
-		model = glm::mat4(1.0);
-		
-		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 3.0f));
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, modeloPos);
+		model *= glm::inverse(glm::lookAt(glm::vec3(0.0f), camFront, glm::vec3(0.0f, 1.0f, 0.0f)));
+		modelaux = model;
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		Invincible_M.RenderModel();
+		AtomEve_torso.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(0.0f, 4.642f, -0.04f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		AtomEve_cabeza.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-0.86f, 3.764f, -0.172f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		AtomEve_brazo_der.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(0.899f, 3.647f, -0.304f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		AtomEve_brazo_izq.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-0.594f, 0.26f, -0.052f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		AtomEve_pie_der.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(0.135f, 0.016f, 0.001f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		AtomEve_pie_izq.RenderModel();
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		/*
+		* Interfaz
+		*/
 		
+
+		// Obtener vectores de la cámara
+		glm::vec3 camRight = glm::normalize(glm::cross(camFront, glm::vec3(0.0f, 1.0f, 0.0f)));
+		glm::vec3 camUp = glm::normalize(glm::cross(camRight, camFront));
+
+		// Posicion frente a la camara (a 10 unidades)
+		glm::vec3 basePos = camPos + camFront * 10.0f;
+
+		std::string texto = "FERIA FUTURISTA";
+
+		for (int i = 0; i < texto.size(); i++) {
+			if (texto[i] == ' ') continue;
+
+			float espaciado = 1.0f;
+			glm::vec3 offsetPos = basePos + camRight * (-7.0f + espaciado * i) + camUp * 4.5f;
+
+			// Configurar textura de letras
+			toffset = glm::vec2(offsetFuenteProyU + letrasOffset[texto[i]].first, 
+				(1 - offsetFuenteProyV) + letrasOffset[texto[i]].second);
+
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, offsetPos);
+			model *= glm::inverse(glm::lookAt(glm::vec3(0.0f), camFront, glm::vec3(0.0f, 1.0f, 0.0f)));
+			model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+			glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
+			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+			color = glm::vec3(1.0f, 1.0f, 1.0f);
+			glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+			ProjectDefaultFont.UseTexture();
+			meshList[0]->RenderMesh();
+		}
+
 		glDisable(GL_BLEND);
-
 		glUseProgram(0);
-
 		mainWindow.swapBuffers();
 	}
 
