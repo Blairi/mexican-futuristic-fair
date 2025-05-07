64c834f79bccaf02af2c8a3bcf6f512df7ed43a0
Blairi <axel.avi.fer@gmail.com>
2025-05-07 12:44:25 -0600

    agregando metodo para obtener una matriz

--- mexican-futuristic-fair/ModelJerarquia.cpp
+++ mexican-futuristic-fair/ModelJerarquia.cpp
@@ -123,6 +123,9 @@ void ModelJerarquia::OrientToCamera(glm::vec3 camFront)
 	}
 }
 
+std::vector<glm::mat4> ModelJerarquia::getModelMatrixs() {
+	return this->MatrixModels;
+}
 
 ModelJerarquia::~ModelJerarquia()
 {
